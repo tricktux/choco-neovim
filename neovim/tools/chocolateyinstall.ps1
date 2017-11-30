3342ac8e8ca9306bdd0d19fafedecfdfc30cd3da
Reinaldo Molina <rmolin88@gmail.com>
2017-11-30 12:08:25 -0500

    added checksum to neovim package

--- neovim/tools/chocolateyinstall.ps1
+++ neovim/tools/chocolateyinstall.ps1
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'neovim*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-  checksum      = ''
+  checksum      = 'EDBB17047F9A1BDC01ACF965430EBBC61BD05A9D39668C9CC6961EA47F471C47'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
+  checksum64    = '66BAB53EC0A8AFF854921B6925630026649F3A49BB7913D19A4A602FFC138C92'
   checksumType64= 'sha256' #default is checksumType
 
   validExitCodes= @(0) #please insert other valid exit codes here
