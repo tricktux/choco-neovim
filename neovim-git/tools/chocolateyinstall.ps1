d1d1215fc3e4e5b6cee296ed8827100cd6564e83
Reinaldo Molina <rmolin88@gmail.com>
2018-04-08 22:33:01 -0400

    Adding nvim to path, also attempting to fix documentation

--- neovim-git/tools/chocolateyinstall.ps1
+++ neovim-git/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $destDir = Join-Path $(Get-ToolsLocation) $packageName
 $url        = 'https://github.com/neovim/neovim/releases/download/nightly/nvim-win32.zip' # download url, HTTPS preferred
 $url64      = 'https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
-# Write-Output $destDir
 
+# Write-Output $destDir
 $packageArgs = @{
 	packageName   = $packageName
 	unzipLocation = $destDir
@@ -24,5 +24,23 @@ $packageArgs = @{
 }
 
 Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
+
+$nvim = $destDir + '\Neovim\bin\nvim-qt.exe'
+Install-BinFile -Name 'nvim-qt' -Path $nvim -UseStart true
+$tee = $destDir + "\Neovim\bin\tee.exe"
+Install-BinFile -Name 'tee' -Path $tee
+$tidy = $destDir + "\Neovim\bin\tidy.exe"
+Install-BinFile -Name 'tidy' -Path $tidy
+$winYank = $destDir + "\Neovim\bin\win32yank.exe"
+Install-BinFile -Name 'win32yank' -Path $winYank
+$agent = $destDir + "\Neovim\bin\winpty-agent.exe"
+Install-BinFile -Name 'winpty-agent' -Path $agent
+$cat = $destDir + "\Neovim\bin\cat.exe"
+Install-BinFile -Name 'cat' -Path $cat
+$diff = $destDir + "\Neovim\bin\diff.exe"
+Install-BinFile -Name 'diff' -Path $diff
+$exe = $destDir + "\Neovim\bin\nvim.exe"
+Install-BinFile -Name 'nvim' -Path $exe
+
 Write-Output "Please Consider donating https://salt.bountysource.com/teams/neovim"
 Write-Output "Issues? Please visit https://github.com/neovim/neovim/wiki/Installing-Neovim"
