3a82f1517a390662d8d642cb92c60620530c4ee6
Reinaldo Molina <5428479+tricktux@users.noreply.github.com>
2021-11-25 08:40:02 -0500

    fix: remove Win32 build

--- neovim-git/tools/chocolateyinstall.ps1
+++ neovim-git/tools/chocolateyinstall.ps1
@@ -1,15 +1,12 @@
 ﻿# File:           chocolateyinstall.ps1
 # Description:    Installation of Neovim
 # Author:         Reinaldo Molina
-# Email:          rmolin88 at gmail dot com
-# Revision:       0.0.0
-# Last Modified:  Fri Apr 20 2018 22:47
+# Email:          me at molinamail dot com
 
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'neovim' # arbitrary name for the package, used in messages
 $destDir     = Join-Path $(Get-ToolsLocation) $packageName
-$url         = 'https://github.com/neovim/neovim/releases/download/nightly/nvim-win32.zip' # download url, HTTPS preferred
 $url64       = 'https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 $bin         = $destDir + '\Neovim\bin'
 
@@ -20,13 +17,10 @@ $pp = Get-PackageParameters
 $packageArgs = @{
     packageName   = $packageName
     unzipLocation = $destDir
-    url           = $url
     url64bit      = $url64
 
     softwareName  = 'neovim*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
-    checksum      = ''
-    checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
     checksum64    = ''
     checksumType64= 'sha256' #default is checksumType
 
